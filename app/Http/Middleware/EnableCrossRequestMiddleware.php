ab50e693bcd640e2d65cb00db9469bdd724baf5a
huhao <772496023@qq.com>
2022-07-21 11:40:31 +0800

    解决跨域问题

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -17,10 +17,10 @@ class EnableCrossRequestMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-        $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+//        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+//        $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS, DELETE');
-        $response->header('Access-Control-Allow-Credentials', 'true');
+//        $response->header('Access-Control-Allow-Credentials', 'true');
         return $response;
     }
 }
