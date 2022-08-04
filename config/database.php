5aed8999d36d0f8fdbfd43e6c55ec636169939a3
swei <2557929463@qq.com>
2022-08-04 16:39:29 +0800

    数据库调试

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '139.196.160.101'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ComPDF_test'),
+            'username' => env('DB_USERNAME', 'debian-sys-maint'),
+            'password' => env('DB_PASSWORD', 'tRF4SWujyo6rZ4Qm'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
