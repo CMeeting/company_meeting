4f3e97c7b78509689ff28b153ee3e2cd69a18e4e
DESKTOP-HK8A7ML\kdan <liwenkai@kdanmobile.com>
2022-08-30 17:50:33 +0800

    support模块 - 接口返回参数调整

--- app/Services/SupportapiService.php
+++ app/Services/SupportapiService.php
@@ -33,7 +33,35 @@ class SupportapiService
         if(!$info){
             return ['code'=>'403','msg'=>"没有找到该数据"];
         }
+        switch ($info['status']){
+            case 1:
+                $info['status']="待接收";
+                break;
+            case 2:
+                $info['status']="已接收";
+                break;
+            case 3:
+                $info['status']="已接收";
+                break;
+            case 4:
+                $info['status']="已发布";
+                break;
+        }
+        switch ($info['type']){
+            case 1:
+                $info['type']="Bug";
+                break;
+            case 2:
+                $info['type']="优化";
+                break;
+            case 3:
+                $info['type']="新增";
+                break;
+        }
         $list=$supportlog->_where("order_no='{$data['order_no']}'","created_at desc");
+        foreach ($list as $k=>$v){
+            $list[$k]['info']=explode('&;',$v['info']);
+        }
         $arr['info']=$info;
         $arr['list']=$list;
         return ['code'=>'200','msg'=>"ok",'data'=>$arr];
