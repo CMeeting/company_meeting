d7f7a80a85f2f313af7d9c1ad1b49639806d11b8
DESKTOP-HK8A7ML\kdan <liwenkai@kdanmobile.com>
2022-11-23 21:01:50 +0800

    修复paddle报错

--- app/Services/OrdersService.php
+++ app/Services/OrdersService.php
@@ -30,7 +30,7 @@ use App\Services\MailmagicboardService;
 use App\Services\EmailService;
 use Auth;
 
-require_once  app_path('Http\extend\wechat\lib\WxPay.Data.php');
+
 
 class OrdersService
 {
@@ -868,13 +868,6 @@ class OrdersService
         }
     }
 
-    public function updateorderstatus($trade_no){
-        $order = new Order();
-        $order_goods = new OrderGoods();
-        Db::table("callback_log")->insert(['info' => 'paddle='.$trade_no, 'pay_type' => 1]);
-        $order->_update(['status' => 1, 'pay_time' => date("Y-m-d H:i:s")], "order_no='{$trade_no}'");
-        $order_goods->_update(['status' => 1, 'pay_time' => date("Y-m-d H:i:s")], "order_no='{$trade_no}'");
-    }
 
     function headerurl()
     {
