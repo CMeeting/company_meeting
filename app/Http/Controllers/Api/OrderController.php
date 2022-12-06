7442ba511faa187f006f2cb0e95a69a37bf45122
DESKTOP-HK8A7ML\kdan <liwenkai@kdanmobile.com>
2022-12-06 16:23:11 +0800

    修复paddle支付回调

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -121,10 +121,10 @@ class OrderController
         Db::table("callback_log")->insert(['info' => 'paddle='. json_encode($param), 'pay_type' => 1]);
         if(isset($param['alert_name']) && $param['alert_name']=="payment_succeeded" && isset($param['passthrough'])){
             $merchant_no='paddle'.$param['passthrough'];
-            $orderdata = $order->_find("merchant_no={$merchant_no}");
+            $orderdata = $order->_find("merchant_no='{$merchant_no}'");
             $orderdata = $order->objToArr($orderdata);
             $emaildata = unserialize($orderdata['user_bill']);
-            $ordergoods_data = $ordergoods->_where("merchant_no={$merchant_no}");
+            $ordergoods_data = $ordergoods->_where("merchant_no='{$merchant_no}'");
             $goods_data = $goods->_where("1=1");
             try {
                 DB::table("orders")->whereRaw("order_no='{$param['passthrough']}'")->update(['status' => 1, 'pay_time' => date("Y-m-d H:i:s")]);
