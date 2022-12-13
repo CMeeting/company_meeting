1b599b1ebb97192048f36201dc5c23cfbac6c64d
pjy <pengjianyong@kdanmobile.com>
2022-12-13 09:39:40 +0800

    订单管理-paddle支付还原

--- app/Http/Controllers/Api/biz/PaddleBiz.php
+++ app/Http/Controllers/Api/biz/PaddleBiz.php
@@ -159,11 +159,11 @@ class PaddleBiz
         $orderData = [
             'vendor_id'         =>  $this->config['vendor_id'],
             'vendor_auth_code'  =>  $this->config['vendor_auth_code'],
-            'product_id'   =>  41223, //产品ID
-            'title'   =>  'PDF Conversion SDK', //产品名称
+            'product_id'   =>  14228, //产品ID
+            'title'   =>  $product, //产品名称
             'prices' =>  ['USD:' . $price],
             'quantity_variable'   =>  '0', //用户修改更改购买数量配置
-//            'return_url'=> 'http://test-pdf-pro.kdan.cn:3026/order/checkout/payed?out_trade_no=paddle'.$order,
+            'return_url'=> 'http://test-pdf-pro.kdan.cn:3026/order/checkout/payed?out_trade_no=paddle'.$order,
             'discountable' => '0',
             'quantity'   =>  $amount ?? '1', //购买数量默认为1个
 //            'expires'   =>  '2021-08-29', //支付链接过期时间
