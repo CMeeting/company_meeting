97c2c5c53fc745f03ccf6132c97c056449b6793b
pjy <pengjianyong@kdanmobile.com>
2022-12-14 15:02:07 +0800

    订单管理-订单详情返回账单信息

--- app/Services/OrdersService.php
+++ app/Services/OrdersService.php
@@ -498,19 +498,23 @@ class OrdersService
         }
         $ordergoodsdata = $orderGoods
             ->leftJoin('goods', 'orders_goods.goods_id', '=', 'goods.id')
+            ->leftJoin('orders', 'orders.id', '=','orders_goods.order_id')
             ->whereRaw("orders_goods.order_id='{$pram['order_id']}'")
-            ->selectRaw("orders_goods.appid,orders_goods.goods_no,orders_goods.pay_type,orders_goods.status,orders_goods.price,orders_goods.id,goods.level1,goods.level2,goods.level3,orders_goods.pay_years period")
+            ->selectRaw("orders_goods.appid,orders_goods.goods_no,orders_goods.pay_type,orders_goods.status,orders_goods.price,orders_goods.id,goods.level1,goods.level2,goods.level3,orders_goods.pay_years period,orders.user_bill")
             ->get()->toArray();
         if (!empty($ordergoodsdata)) {
             $classification = $this->assembly_orderclassification();
             foreach ($ordergoodsdata as $k => $v) {
-                 $ordergoodsdata[$k]['products'] = isset($classification[$v['level1']]['title']) ? $classification[$v['level1']]['title'] : "";
-                 $ordergoodsdata[$k]['platform'] = isset($classification[$v['level2']]['title']) ? $classification[$v['level2']]['title'] : "";
-                 $ordergoodsdata[$k]['licensie'] = isset($classification[$v['level3']]['title']) ? $classification[$v['level3']]['title'] : "";
-                 $license_code=$LicenseModel->_find("ordergoods_id=".$v['id']);
-                 $license_code=$LicenseModel->objToArr($license_code);
+                $ordergoodsdata[$k]['products'] = isset($classification[$v['level1']]['title']) ? $classification[$v['level1']]['title'] : "";
+                $ordergoodsdata[$k]['platform'] = isset($classification[$v['level2']]['title']) ? $classification[$v['level2']]['title'] : "";
+                $ordergoodsdata[$k]['licensie'] = isset($classification[$v['level3']]['title']) ? $classification[$v['level3']]['title'] : "";
+                $license_code=$LicenseModel->_find("ordergoods_id=".$v['id']);
+                $license_code=$LicenseModel->objToArr($license_code);
                 $ordergoodsdata[$k]['expire_time']=$license_code['expire_time'];
                 $ordergoodsdata[$k]['created_at']=$license_code['created_at'];
+
+                $ordergoodsdata[$k]['user_bill'] = $ordergoodsdata[$k]['user_bill'] ? unserialize($ordergoodsdata[$k]['user_bill']) : '';
+
                 switch ($v['pay_type']) {
                     case 1:
                         $ordergoodsdata[$k]['payname'] = "paddle";
