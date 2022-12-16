26155d0cb78ec5f591b96a2c11f7088f1ee0a96e
pjy <pengjianyong@kdanmobile.com>
2022-12-16 17:05:28 +0800

    订单-请求paddle支付链接日志

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('command:close:order')->dailyAt('16:55');
+        $schedule->command('command:close:order')->dailyAt('9:10');
     }
 
     /**
