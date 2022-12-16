04c4abc0d325e182b7b07f464ead0b9bd32c3304
pjy <pengjianyong@kdanmobile.com>
2022-12-16 16:49:55 +0800

    订单管理-订单关闭脚本调试

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('command:close:order')->dailyAt('7:25');
+        $schedule->command('command:close:order')->dailyAt('16:55');
     }
 
     /**
