f9ca0885f72a382f351ca5c7b0b06c933c6d42cc
pjy <pengjianyong@kdanmobile.com>
2023-01-03 09:49:40 +0800

    邮件模板-日期格式化函数

---
+++ app/Services/CommonService.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Carbon\Carbon;
+
+class CommonService
+{
+    public static function formatDate($date){
+        $months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        $date_obj = Carbon::parse($date);
+        $month  = $date_obj->month;
+        $month_index = $month-1;
+        $month_en = $months[$month_index];
+        $format = $date_obj->format('d, Y, H：i');
+
+        return $month_en . ' ' . $format;
+    }
+}
