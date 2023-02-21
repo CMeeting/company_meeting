4edd9f4b732612284222962ffd9eed610282adb1
swei <2557929463@qq.com>
2023-02-21 15:16:47 +0800

    reCAPTCHA:调试样式

--- resources/views/admin/admins/login.blade.php
+++ resources/views/admin/admins/login.blade.php
@@ -39,7 +39,7 @@
             </div>
             <div class="col-sm-7 animated fadeInRight">
                 <form method="post" action="{{route('login-handle')}}">
-                    <div class="g-recaptcha" data-sitekey="6LcbEZ4kAAAAAP2F6g29yX_TI8Pgsaic-G03ZlZ-"></div>
+                    <div class="g-recaptcha" data-sitekey="6LcbEZ4kAAAAACuKdwPOomSWNye--wMTIDoT5hKg"></div>
 {{--                    {{csrf_field()}}--}}
 {{--                    <p class="login-title">登录</p>--}}
 {{--                    <p class="m-t-md" style="color:#666">登录到{{ config('app.name', 'Laravel') }}系统后台管理</p>--}}
